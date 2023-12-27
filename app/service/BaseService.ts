ee42757ca35c55c69af4c255e242bc79f465ad29
JackieTruongZ <datnguyentruongnn@gmail.com>
2023-12-27 23:20:47 +0700

    fix login

--- app/service/BaseService.ts
+++ app/service/BaseService.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const API_URL = "https://magicpost-60b7.onrender.com";
+const API_URL = "https://magicpost-183b.onrender.com";
 // const API_URL = "http://localhost:3333";
 
 export class BaseService {
